d261d9d413e5a36c3eef31c66f3cec011943cad8
Jaffar Abbas <gamaportal8@gmail.com>
2021-12-02 22:10:57 +0500

    02/12/2021 22:10:52

---
+++ effort_estimation_app/lib/containers/_text.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors, non_constant_identifier_names, unused_element
+
+import 'package:flutter/material.dart';
+
+Widget TextArea(String str) => Text(
+      str,
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 40,
+        fontWeight: FontWeight.bold,
+      ),
+    );
