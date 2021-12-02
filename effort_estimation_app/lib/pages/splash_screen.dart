3fff26896797a16d7557e6e533fe5fc76fd0cd17
Jaffar Abbas <gamaportal8@gmail.com>
2021-12-02 22:45:30 +0500

    02/12/2021 22:45:30

---
+++ effort_estimation_app/lib/pages/splash_screen.dart
@@ -0,0 +1,47 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:async';
+
+import 'package:effort_estimation_app/containers/_text.dart';
+import 'package:effort_estimation_app/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  SplashScreen({ Key? key }) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+ @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(
+      Duration(seconds: 3),
+      () => {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        ),
+      },
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+             TextArea('EFFORT'),
+             TextArea('ESTIMATION'),
+             TextArea('APP'),
+          ],
+        ),
+      ),
+    );
+  }
+}
