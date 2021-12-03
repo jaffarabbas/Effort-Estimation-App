d00ec1e3124a791f7b295ae0b8cb69d81900a34f
Jaffar Abbas <gamaportal8@gmail.com>
2021-12-03 15:17:58 +0500

    03/12/2021 15:17:57

--- effort_estimation_app/lib/main.dart
+++ effort_estimation_app/lib/main.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:effort_estimation_app/pages/functional_point.dart';
 import 'package:effort_estimation_app/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: FunctionalPoint(),
     );
   }
 }
