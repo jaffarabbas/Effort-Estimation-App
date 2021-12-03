5b8fa69a2e35eba71acf17fa3e61ef914e68949c
Jaffar Abbas <gamaportal8@gmail.com>
2021-12-03 15:17:58 +0500

    03/12/2021 15:17:57

--- effort_estimation_app/lib/pages/splash_screen.dart
+++ effort_estimation_app/lib/pages/splash_screen.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 
 import 'package:effort_estimation_app/containers/_text.dart';
 import 'package:effort_estimation_app/pages/home_page.dart';
+import 'package:effort_estimation_app/widgits/mediaQuery.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -30,19 +31,15 @@ class _SplashScreenState extends State<SplashScreen> {
   }
   @override
   Widget build(BuildContext context) {
-    final height = MediaQuery.of(context).size.height -
-        MediaQuery.of(context).padding.top -
-        kToolbarHeight;
-    final width = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Colors.blueAccent,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-             TextArea('EFFORT',width * 0.1),
-             TextArea('ESTIMATION',width * 0.1),
-             TextArea('APP',width * 0.1),
+             TextArea('EFFORT',Resposive.ResponsiveWidth(context, 0.1)),
+             TextArea('ESTIMATION',Resposive.ResponsiveWidth(context, 0.1)),
+             TextArea('APP',Resposive.ResponsiveWidth(context, 0.1)),
           ],
         ),
       ),
